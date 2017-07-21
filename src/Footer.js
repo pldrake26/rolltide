1d733c9c957e75b4d92bd09ab6887a1c41415aca
pldrake26 <pldrake26@outlook.com>
2017-07-21 02:59:35 -0500

    Changed footer content and rebuilt app.

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
         return (
             <div>
                 <p>&copy;2017 - Coded by Phillip L. Drake using React. - Logo is a trademark of the University of Alabama.</p>
-                <p>In memory of Joe and Lucy Morris.</p>
+                <p>In memory of Troy Perigen, Joe and Lucy Morris, and their love of Alabama football.</p>
             </div>
         );
     }
