ebce4a0cec6cca78ab8cbe1183e9c2b09ca3e73f
pldrake26 <pldrake26@outlook.com>
2017-07-21 02:46:35 -0500

    Added content to Footer and rebuilt the app.

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 export default class Footer extends Component {
     render() {
         return (
-            <p>&copy;2017 - Coded by Phillip L. Drake using React. - Logo is a trademark of the University of Alabama.</p>
+            <div>
+                <p>&copy;2017 - Coded by Phillip L. Drake using React. - Logo is a trademark of the University of Alabama.</p>
+                <p>In memory of Joe and Lucy Morris.</p>
+            </div>
         );
     }
 }
