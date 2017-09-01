72a143f879a1d24f7d2a36bd35fdcf34bbc5b198
Phillip Drake <pldrake26@outlook.com>
2017-08-31 22:34:44 -0500

    Footer style and content changes.

--- src/Footer.js
+++ src/Footer.js
@@ -4,20 +4,20 @@ import styled from 'styled-components';
 const PageFooter = styled.div`
   background-color: #A60B35;
   color: white;
-  font-weight: bold;
   padding: 1%;
-  margin-top: 1%;
 `
 
 export default class Footer extends Component {
   render() {
     return (
-      <PageFooter>
-        <p>&copy;2017 - Coded by Phillip L. Drake using React.</p>
-        <p>Logo is a trademark of the University of Alabama.<br/>
-        Helmets are trademarks of the individual colleges and the NCAA.</p>
-        <p>In memory of Troy Perigen, Joe and Lucy Morris, and their love of Alabama football.</p>
-      </PageFooter>
+        <PageFooter className="row">
+          <div className="col-sm-12">
+            <p>&copy;2017 - Coded by Phillip L. Drake using React.</p>
+            <p>Logo is a trademark of the University of Alabama.<br/>
+            Helmets are trademarks of the individual colleges and the NCAA.</p>
+            <p>In memory of Troy Perigen, Joe and Lucy Morris, and their love of Alabama football.</p>
+          </div>
+        </PageFooter>
     );
   }
 }
