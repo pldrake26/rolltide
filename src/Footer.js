e1c801bb4c5fc82b53227d1e33334c3bc7cd581d
Phillip Drake <pldrake26@outlook.com>
2017-09-01 21:34:26 -0500

    New trademark disclaimer added and rebuild.

--- src/Footer.js
+++ src/Footer.js
@@ -13,8 +13,7 @@ export default class Footer extends Component {
         <PageFooter className="row">
           <div className="col-sm-12">
             <p>&copy;2017 - Coded by Phillip L. Drake using React.</p>
-            <p>Logo is a trademark of the University of Alabama.<br/>
-            Helmets are trademarks of the individual colleges and the NCAA.</p>
+            <p>All logos and helmets are trademarks™ or registered® trademarks of their respective holders.<br/>Use of them does not imply any affiliation with or endorsement by them.</p>
             <p>In memory of Troy Perigen, Joe and Lucy Morris, and their love of Alabama football.</p>
           </div>
         </PageFooter>
